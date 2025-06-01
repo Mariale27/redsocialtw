706c857b64fb09433ad1d58961c4fa516955798d
ANCRARD <andres23anaya@gmail.com>
2025-06-01 15:12:06 -0500

    nuevo repositorio

---
+++ src/test/java/com/redsocial/redsocialpro/RedsocialproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redsocial.redsocialpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedsocialproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
